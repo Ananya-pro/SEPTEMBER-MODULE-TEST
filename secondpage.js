0d146c9ca13a1b8b272864370f88a4294d9442a1
Ananya Yaduvanshi <133091353+Ananya-pro@users.noreply.github.com>
2023-10-04 10:30:23 +0530

    Update secondpage.js

--- secondpage.js
+++ secondpage.js
@@ -41,7 +41,7 @@ function getdataonui(data){
         </div>
         <div class="info-div">
             <p>Organisation: <span>${data.org}</span></p>
-            <p>Hostname: <span>${hostname}</span></p>
+            <p>Hostname: <span>${data.asn}</span></p>
         </div> `;
         const mapIframe = document.getElementById('location-map');
         mapIframe.src = `https://maps.google.com/maps?q=${lat},${long}&z=15&output=embed`;
